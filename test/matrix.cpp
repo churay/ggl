720faa315e0c5edc219269f2024c736222d43dd3
Joseph Ciurej <churayy@gmail.com>
2016-02-20 23:44:54 -0800

    Implemented the test cases for the default "ggl::matrix" constructor.

--- test/matrix.cpp
+++ test/matrix.cpp
@@ -1,5 +1,29 @@
 #include "src/matrix.h"
 
-TEST_CASE( "C++ Integer Equality Works", "[c++][int]" ) {
-    REQUIRE( 1 == 1 );
+SCENARIO( "ggl::matrix is correctly initialized", "[matrix]" ) {
+    GIVEN( "no parameters (default constructor)" ) {
+        // TODO(JRC): For some reason, using "m.sNumRows" in an assertion
+        // causes an undefined reference error.
+        WHEN( "the dimensions are trivial" ) {
+            const ggl::matrixf<1, 1> m;
+            const unsigned r = m.sNumRows, c = m.sNumCols, e = m.sNumEnts;
+            THEN( "the dimensions are correct" ) {
+                REQUIRE( r == 1 ); REQUIRE( c == 1 ); REQUIRE( e == 1 );
+            } THEN( "the elements are all zero" ) {
+                REQUIRE( m(0, 0) == Approx(0.0f) );
+            }
+        }
+
+        WHEN( "the dimensions are nontrivial" ) {
+            const ggl::matrixf<2, 3> m;
+            const unsigned r = m.sNumRows, c = m.sNumCols, e = m.sNumEnts;
+            THEN( "the dimensions are correct" ) {
+                REQUIRE( r == 2 ); REQUIRE( c == 3 ); REQUIRE( e == 6 );
+            } THEN( "the elements are all zero" ) {
+                for( unsigned rIdx = 0; rIdx < r; ++rIdx )
+                    for( unsigned cIdx = 0; cIdx < c; ++cIdx )
+                        REQUIRE( m(rIdx, cIdx) == Approx(0.0f) );
+            }
+        }
+    }
 }
