e08bc1a000ef672ab64eff97a09bdae847a5a32f
Joseph Ciurej <churayy@gmail.com>
2016-04-13 19:39:46 -0700

    Implemented test case stubs for all of the functions in 'ggl::xform'.

---
+++ test/xform.cpp
@@ -0,0 +1,42 @@
+#include "catch.hpp"
+#include "src/xform.hpp"
+
+SCENARIO( "ggl::xform basis operation works", "[xform][stub]" ) {
+    GIVEN( "" ) {
+        WHEN( "" ) {
+            THEN( "" ) {
+                REQUIRE( 1 != 1 );
+            }
+        }
+    }
+}
+
+SCENARIO( "ggl::xform scale operation works", "[xform][stub]" ) {
+    GIVEN( "" ) {
+        WHEN( "" ) {
+            THEN( "" ) {
+                REQUIRE( 1 != 1 );
+            }
+        }
+    }
+}
+
+SCENARIO( "ggl::xform translate operation works", "[xform][stub]" ) {
+    GIVEN( "" ) {
+        WHEN( "" ) {
+            THEN( "" ) {
+                REQUIRE( 1 != 1 );
+            }
+        }
+    }
+}
+
+SCENARIO( "ggl::xform rotate operation works", "[xform][stub]" ) {
+    GIVEN( "" ) {
+        WHEN( "" ) {
+            THEN( "" ) {
+                REQUIRE( 1 != 1 );
+            }
+        }
+    }
+}
