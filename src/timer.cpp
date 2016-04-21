5d2c43bc0c62b5191cd6afcef9a5c82d065f179e
Joseph Ciurej <churayy@gmail.com>
2016-04-20 22:16:05 -0700

    Added frames per second monitoring support to the 'ggl::timer' type.

--- src/timer.cpp
+++ src/timer.cpp
@@ -7,10 +7,11 @@
 namespace ggl {
 
 timer::timer( const size_t pFPS ) {
-    std::chrono::duration<long double, std::ratio<1>> frameDurationSecs( 1.0 / pFPS );
+    SecDuration frameDuration( 1.0 / pFPS );
 
-    mFrameDuration = std::chrono::duration_cast<TimeDuration>( frameDurationSecs );
+    mFrameDuration = std::chrono::duration_cast<ClockDuration>( frameDuration );
     mSplitTime = Clock::now();
+    mWaitTime = Clock::now();
 }
 
 
@@ -20,11 +21,24 @@ void timer::split() {
 
 
 void timer::wait() {
-    auto currTime = Clock::now();
-    auto elapsedTime = currTime - mSplitTime;
-    auto remainingTime = mFrameDuration - elapsedTime;
+    mWaitTime = Clock::now();
 
+    auto elapsedTime = mWaitTime - mSplitTime;
+    auto remainingTime = mFrameDuration - elapsedTime;
     std::this_thread::sleep_for( remainingTime );
+}
+
+
+size_t timer::prevFrameFPS() const {
+    SecDuration prevFrameTime = std::chrono::duration_cast<SecDuration>( mWaitTime - mSplitTime );
+    return static_cast<size_t>( 1.0 / prevFrameTime.count() );
+}
+
+
+size_t timer::avgFrameFPS() const {
+    // TODO(JRC): Implement this function so that it properly outputs the average
+    // FPS for the past several frames.
+    return (*this).prevFrameFPS();
 }
 
 }
--- src/timer.h
+++ src/timer.h
@@ -2,12 +2,13 @@
 #define GGL_TIMER_H
 
 #include <chrono>
+#include <ratio>
 
 namespace ggl {
 
 class timer {
     public:
-    
+
     /// Constructors ///
 
     timer( const size_t pFPS = 60 );
@@ -17,18 +18,22 @@ class timer {
     void split();
     void wait();
 
+    size_t prevFrameFPS() const;
+    size_t avgFrameFPS() const;
+
     private:
 
     /// Class Setup ///
 
     using Clock = std::chrono::high_resolution_clock;
-    using TimePoint = decltype( Clock::now() );
-    using TimeDuration = decltype( Clock::now() - Clock::now() );
+    using ClockPoint = decltype( Clock::now() );
+    using ClockDuration = decltype( Clock::now() - Clock::now() );
+    using SecDuration = std::chrono::duration<long double, std::ratio<1>>;
 
     /// Class Fields ///
 
-    TimeDuration mFrameDuration;
-    TimePoint mSplitTime;
+    ClockDuration mFrameDuration;
+    ClockPoint mSplitTime, mWaitTime;
 };
 
 }
